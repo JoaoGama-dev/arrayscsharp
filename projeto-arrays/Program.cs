641623b81e5abc34f0f53131aa58258548cd004e
JoaoGama-dev <joaogamahjj@gmail.com>
2020-09-02 13:32:32 -0300

    finalizado.tarefas com Arrays.

--- projeto-arrays/Program.cs
+++ projeto-arrays/Program.cs
@@ -10,10 +10,19 @@ namespace projeto_arrays
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Realize a soma abaixo");
+            Console.WriteLine("Criando Arrays, resolução do exercício 1.");
 
-            int[] numeros_soma;
-                numeros_soma = new int[7];
+            double[] numeros = new double[7];
+            numeros[1] = 1;
+            numeros[2] = 2;
+            numeros[3] = 3;
+            numeros[4] = 4;
+            numeros[5] = 10;
+            numeros[6] = 11;
+            double soma = numeros[1] + numeros[2] + numeros[3] + numeros[4] + numeros[5] + numeros[6];
+
+            Console.WriteLine("Soma é " + soma , "tecle ENTER para sair...");
+            Console.ReadKey();
 
         }
     }
