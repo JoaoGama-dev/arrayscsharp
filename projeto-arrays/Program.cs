571c5ad2a2312514c99cea957bbb8fd45a53de1a
JoaoGama-dev <joaogamahjj@gmail.com>
2020-09-02 10:17:02 -0300

    Adicionar arquivos de projeto.

---
+++ projeto-arrays/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace projeto_arrays
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Realize a soma abaixo");
+
+            int[] numeros_soma;
+                numeros_soma = new int[7];
+
+        }
+    }
+}
