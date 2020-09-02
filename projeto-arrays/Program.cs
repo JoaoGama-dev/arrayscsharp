b4970d839438e3acd0d369c6837006f75c2e9831
JoaoGama-dev <joaogamahjj@gmail.com>
2020-09-02 17:11:57 -0300

    Exercicio 2 feito!

--- projeto-arrays/Program.cs
+++ projeto-arrays/Program.cs
@@ -21,9 +21,28 @@ namespace projeto_arrays
             numeros[6] = 11;
             double soma = numeros[1] + numeros[2] + numeros[3] + numeros[4] + numeros[5] + numeros[6];
 
-            Console.WriteLine("Soma é " + soma , "tecle ENTER para sair...");
-            Console.ReadKey();
+            Console.WriteLine(" A soma é " + soma );
 
+            Console.WriteLine(" Exercicio 2, favor vá teclando ENTER até a resolução ");
+            for (int i = 0; i < 101; i++)
+            {
+                if (i % 3 == 0)
+                {
+                    Console.Write("fizz ");
+                }
+                if (i % 5 == 0)
+                {
+                    Console.Write("buzz ");
+                }
+                if (i % 3 != 0 && i % 5 != 0)
+                {
+                    Console.Write(i);
+                }
+                Console.WriteLine();
+
+                Console.ReadKey();
+
+            }
         }
     }
 }
